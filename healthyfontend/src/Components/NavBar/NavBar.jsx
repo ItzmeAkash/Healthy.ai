6bbd380cd8198580ddce94d0f62a63e93f2fad50
Akash Ps <itzmeakashps@gmail.com>
2024-01-22 17:07:26 +0530

    responsive created

--- healthyfontend/src/Components/NavBar/NavBar.jsx
+++ healthyfontend/src/Components/NavBar/NavBar.jsx
@@ -2,30 +2,58 @@ import React, { useState } from 'react'
 import logo from '../../assets/logo.png'
 import './NavBar.css'
 import { Link } from 'react-router-dom'
+import { FaBars } from "react-icons/fa";
 const NavBar = () => {
 
   const [menu, setMenu] = useState("Home")
+  const [isMenuVisible, setMenuVisible] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuVisible(!isMenuVisible);
+  };
+  const handleMenuItemClick = (menuItem) => {
+    setMenu(menuItem);
+    setMenuVisible(false);
+  };
+
   return (
     <>
       <div className="navbar">
         <div className="nav-log">
           <img src={logo} alt="" />
         </div>
-        <ul className="nav-menu">
 
-          <li onClick={() => { setMenu("Home") }}> <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>Home</Link> {menu === "Home" ? <hr /> : <></>}</li>
-          <li onClick={() => { setMenu("About Us") }}><Link style={{ textDecoration: 'none', color: 'black' }} to='/aboutus'>About Us</Link>{menu === "About Us" ? <hr /> : <></>} </li>
-          <li onClick={() => { setMenu("Services") }}><Link style={{ textDecoration: 'none', color: 'black' }} to='/services'>Services</Link> {menu === "Services" ? <hr /> : <></>} </li>
-          <li onClick={() => { setMenu("Contact Us") }}><Link style={{ textDecoration: 'none', color: 'black' }} to='/services'>Contact Us</Link> {menu === "Contact Us" ? <hr /> : <></>} </li>
-          <li></li>
+        <ul className={`nav-menu ${isMenuVisible ? 'visible' : ''}`}>
+          <li onClick={() => handleMenuItemClick('Home')}>
+            <Link style={{textDecoration:'none', color:'black'}} to="/">Home</Link>
+            {menu === 'Home' ? <hr /> : <></>}
+          </li>
+          <li onClick={() => handleMenuItemClick('About Us')}>
+            <Link style={{textDecoration:'none', color:'black'}} to="/aboutus">About Us</Link>
+            {menu === 'About Us' ? <hr /> : <></>}
+          </li>
+          <li onClick={() => handleMenuItemClick('Services')}>
+            <Link style={{textDecoration:'none', color:'black'}} to="/services">Services</Link>
+            {menu === 'Services' ? <hr /> : <></>}
+          </li>
+          <li onClick={() => handleMenuItemClick('Contact Us')}>
+            <Link style={{textDecoration:'none', color:'black'}} to="/contactus">Contact Us</Link>
+            {menu === 'Contact Us' ? <hr /> : <></>}
+          </li>
         </ul>
+
         <div className="nav-login">
-          <Link to='/login'><button>Login</button></Link>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
         </div>
+
+        <button className="menu-bars" onClick={handleMenuToggle}>
+          <FaBars />
+        </button>
       </div>
     </>
+  );
+};
 
-  )
-}
-
-export default NavBar
+export default NavBar;
