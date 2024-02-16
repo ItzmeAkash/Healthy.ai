1a0a8eacf0ab37588a80335c787129c66b5b565f
Akash Ps <itzmeakashps@gmail.com>
2024-02-16 15:07:46 +0530

    foodimagge classification model user section updated

--- backend/serviceapp/models.py
+++ backend/serviceapp/models.py
@@ -32,7 +32,10 @@ class DietRecommendation(models.Model):
 # Model for Food Image Classification
 
 class FoodImageModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='food_images')
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    
+
+    def __str__(self):
+        return  self.user.first_name
     
